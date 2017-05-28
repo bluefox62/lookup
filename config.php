30f175d8587973ccb819dc654cfef285e67524a3
bluefox62 <brett.riegert@smail.rasmussen.edu>
2017-05-28 17:47:50 -0500

    c

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// This is an example of config.php
+$servername = 'us-cdbr-azure-southcentral-f.cloudapp.net';
+$username = 'b7dfeab7c0eacf';
+$password = 'de68d289';
+$dbname = 'lookup62';
+?>
